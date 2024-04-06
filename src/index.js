a861f5c3f3b85360510f09a2f1e1135197a87658
Uwes Ahabab <uwesahabab@gmail.com>
2024-04-06 17:31:34 +0300

    ....

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+document.addEventListener("DOMContentLoaded",
+function(){
+    function loadDogImages(){
+        fetch(imgUrl)
+            .then((response)=> response.json())
+            .then((data)=>{
+                const images=data["message"] 
+                images.forEach(element => {
+                    const dogImages=document.createElement('img');
+                    const dogImageContaainer=document.querySelector("#dog-image-container")
+                    dogImages.src=element;  
+                    dogImageContaainer.appendChild(dogImages); 
+                    
+                })    
+            })
+    
+    }
+    function loadDogBreed(){
+        fetch(breedUrl).then(response=>response.json()).then(
+            (data)=>{
+                const breeds= data["message"];
+                for(breed in breeds){
+                    const dogBreed=document.createElement('li');
+                    const dogBreeds=document.querySelector("#dog-breeds")
+                    dogBreed.innerText=breed;  
+                    dogBreeds.appendChild(dogBreed)
+                    dogBreed.addEventListener('click', ()=>{
+                    dogBreed.style.color="red"
+                    const options=document.querySelector("#breed-dropdown")
+                    options.addEventListener('change',function(event){
+                        
+                    })
+
+                    })      
+                }
+
+
+            }
+        )
+
+    }
+    loadDogImages()
+    loadDogBreed()
+}
+)
